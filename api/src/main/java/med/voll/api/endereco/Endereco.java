4286130547bfed584c5887fe9884fdf3c4649b5c
anderson029 <andersonfoliveira@yahoo.com>
2023-10-11 01:42:25 -0300

    aula03.05

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,21 @@
+package med.voll.api.endereco;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String numero;
+    private String complemento;
+}
